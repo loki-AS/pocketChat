fb58c34bf1ed7ae77b7d37570a53f14678f81cae
loki <lohithgowda6617@gmail.com>
2022-12-26 12:32:15 +0530

    local commit

--- pages/index.js
+++ pages/index.js
@@ -97,7 +97,6 @@ export default function Home({ messages }) {
                 )) : <NoResults />
               } 
               
-              
               <div ref={messageRef} /> 
 
               </div>
